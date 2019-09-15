61fc75907570072040b78afd49ec7cd2f3dd156d
Michal Mazurek <akfaew@gmail.com>
2019-09-15 12:29:10 +0200

    Add FulfillmentWebhook{}

--- subscription_event.go
+++ subscription_event.go
@@ -12,6 +12,13 @@ import (
 	"sort"
 )
 
+// https://developer.paddle.com/webhook-reference/product-fulfillment/fulfillment-webhook
+type FulfillmentWebhook struct {
+	EventTime   string `json:"event_time"`
+	Quantity    int    `json:"quantity"`
+	Passthrough string `json:"passthrough"`
+}
+
 // https://paddle.com/docs/subscriptions-event-reference/#subscription_created
 type SubscriptionCreated struct {
 	SubscriptionID     string `json:"subscription_id"`
@@ -141,4 +148,43 @@ func ValidatePayload(r *http.Request, pubkey *rsa.PublicKey) (interface{}, error
 	default:
 		return nil, nil
 	}
+}
+
+// https://paddle.com/docs/reference-verifying-webhooks/
+// FulfillmentWebhook does not have an alert_type, so handle it in a separate url
+func ValidateFulfillmentWebhookPayload(r *http.Request, pubkey *rsa.PublicKey) (*FulfillmentWebhook, error) {
+	payload := map[string]string{}
+
+	// Get the p_signature parameter and base64 decode it.
+	if err := r.ParseForm(); err != nil {
+		return nil, err
+	}
+	p_signature := r.Form.Get("p_signature")
+	signature, err := base64.StdEncoding.DecodeString(p_signature)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the fields sent in the request, and remove the p_signature parameter
+	r.Form.Del("p_signature")
+	for k := range r.Form {
+		payload[k] = r.Form.Get(k) // r.Form is a map[string][]string
+	}
+
+	// ksort() and serialize the fields
+	hashed := sha1.Sum(phpserialize(r.Form))
+
+	if err = rsa.VerifyPKCS1v15(pubkey, crypto.SHA1, hashed[:], signature); err != nil {
+		return nil, err
+	}
+
+	ret := new(FulfillmentWebhook)
+	if j, err := json.Marshal(payload); err != nil {
+		return nil, err
+	} else {
+		if err := json.Unmarshal(j, ret); err != nil {
+			return nil, err
+		}
+	}
+	return ret, nil
 }

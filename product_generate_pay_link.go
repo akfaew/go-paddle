c349f7a2b16d62a03d72489818dbf3d0334825ae
Michal Mazurek <akfaew@gmail.com>
2020-06-24 09:31:06 +0200

    Update ProductGeneratePayLinkOptions based on the new documentation

--- product_generate_pay_link.go
+++ product_generate_pay_link.go
@@ -13,33 +13,39 @@ type ProductPayLinkResponse struct {
 	Response ProductPayLink `json:"response"`
 }
 
+// https://developer.paddle.com/api-reference/product-api/pay-links/createpaylink
 type ProductGeneratePayLinkOptions struct {
 	VendorID       int    `url:"vendor_id"`
 	VendorAuthCode string `url:"vendor_auth_code"`
 
-	// https://paddle.com/docs/api-custom-checkout/
 	ProductID               int      `url:"product_id,omitempty"`
 	Title                   string   `url:"title,omitempty"`
 	WebhookURL              string   `url:"webhook_url,omitempty"`
 	Prices                  []string `url:"prices,brackets,omitempty"`
-	Locale                  string   `url:"locale,omitempty"`
 	RecurringPrices         []string `url:"recurring_prices,brackets,omitempty"`
 	TrialDays               int      `url:"trial_days,omitempty"`
 	CustomMessage           string   `url:"custom_message,omitempty"`
 	CouponCode              string   `url:"coupon_code,omitempty"`
+	Discountable            int      `url:"discountable,omitempty"`
 	ImageURL                string   `url:"image_url,omitempty"`
 	ReturnURL               string   `url:"return_url,omitempty"`
 	QuantityVariable        int      `url:"quantity_variable,omitempty"`
 	Quantity                int      `url:"quantity,omitempty"`
 	Expires                 string   `url:"expires,omitempty"`
-	Affiliates              string   `url:"affiliates,omitempty"`
-	RecurringAffiliateLimit string   `url:"recurring_affiliate_limit,omitempty"`
+	Affiliates              []string `url:"affiliates,omitempty"`
+	RecurringAffiliateLimit int      `url:"recurring_affiliate_limit,omitempty"`
 	MarketingConsent        int      `url:"marketing_consent,omitempty"`
 	CustomerEmail           string   `url:"customer_email,omitempty"`
 	CustomerCountry         string   `url:"customer_country,omitempty"`
 	CustomerPostcode        string   `url:"customer_postcode,omitempty"`
-	VatCode                 string   `url:"vat_code,omitempty"`
 	Passthrough             string   `url:"passthrough,omitempty"`
+	VatNumber               string   `url:"vat_number,omitempty"`
+	VatCompanyName          string   `url:"vat_company_name,omitempty"`
+	VatStreet               string   `url:"vat_street,omitempty"`
+	VatCity                 string   `url:"vat_city,omitempty"`
+	VatState                string   `url:"vat_state,omitempty"`
+	VatCountry              string   `url:"vat_country,omitempty"`
+	VatPostcode             string   `url:"vat_postcode,omitempty"`
 }
 
 func (s *ProductService) GeneratePayLink(ctx context.Context, options *ProductGeneratePayLinkOptions) (*ProductPayLinkResponse, error) {
